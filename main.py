8602d4a39620212840399695a6741622db3f1e45
Martin Jaime Flores Jr <mf34314@gmail.com>
2018-11-24 21:40:01 -0600

    Reads test files
    Needs to be refactored, one hot function needs to be fixed.

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ import tensorflow as tf
 
 trainPath = os.environ['IRMAS_TRAIN']
 testPath = os.environ['IRMAS_TEST']
-trainFolders = ('cel/', 'cla/', 'flu/')
-testFolders = ('part1/', 'part2/', 'part3/')
-# , 'gac/', 'gel/', 'org/', 'pia/', 'sax/', 'tru/', 'vio/', 'voi/')
+trainFolders = ('cel/', 'cla/', 'flu/') # ('gac/', 'gel/', 'org/', 'pia/', 'sax/', 'tru/', 'vio/', 'voi/')
+testFolders = ('Part1/', 'Part2/') # ('part2/', 'part3/')
 
 
 def list_files(path):
@@ -21,7 +20,7 @@ def list_files(path):
 
 
 def extract_features(file, folder):
-    y, sr = librosa.load(dataPath + folder + file, mono=True)
+    y, sr = librosa.load(folder + file, mono=True)
     stft = np.abs(librosa.stft(y))
     mfccs = np.mean(librosa.feature.mfcc(y, sr, n_mfcc=40).T, axis=0)
     chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sr).T, axis=0)
@@ -33,15 +32,28 @@ def extract_features(file, folder):
     return mfccs, chroma, mel, contrast, tonnetz
 
 
+def get_label_from_txt(file_path):
+    """
+    Reads text from file at file_path
+    Uses first line as label
+    """
+    with open(file_path, "r") as file:
+        return file.readline(1)
+
+
 def parse_train_files_to_np():
+    """
+    Reads trainPath and tainFolders to parse traning files
+    """
     data = np.empty((0, 193))
     labels = np.empty(0)
-    for folder in folders:
+    for folder in trainFolders:
         files_in_folder = list_files(trainPath + folder)
         print("Extraing data for the " + folder[:-1] + " instrument.")
         for file in files_in_folder:
-            mfccs, chroma, mel, contrast, tonnetz = extract_features(file,
-                                                                     folder)
+            mfccs, chroma, mel, contrast, tonnetz = extract_features(
+                                                            file,
+                                                            trainPath + folder)
             features = np.hstack([mfccs, chroma, mel, contrast, tonnetz])
             data = np.vstack([data, features])
             labels = np.append(labels, folder[:-1])
@@ -49,11 +61,34 @@ def parse_train_files_to_np():
 
 
 def parse_test_files_to_np():
+    """
+    Reads testPath and testFolder to parse test folders
+    """
     data = np.empty((0, 193))
     labels = np.empty(0)
+
+    for folder in testFolders:
+        files_in_folder = list_files(testPath + folder)
+
+        proper_files = []
+
+        for file in files_in_folder:
+            if file.endswith(".txt"):
+                proper_files.append(file[:-4])
+
+        for file in proper_files:
+            mfccs, chroma, mel, contrast, tonnetz = extract_features(
+                                                            file + ".wav",
+                                                            testPath + folder)
+            features = np.hstack([mfccs, chroma, mel, contrast, tonnetz])
+            data = np.vstack([data, features])
+            labels = np.append(labels, get_label_from_txt(
+                                            testPath + folder + file + ".txt"))
+
     return data, labels
 
 
+# change so each label has the same enoding (a = [0, 1], b = [1, 0])
 def one_hot_encode(labels):
     enc = LabelBinarizer()
     return enc.fit_transform(labels)
@@ -66,7 +101,7 @@ def MNN(train_x, train_y, test_x, test_y):
     print("\nThe shape of X: ", train_x.shape)
     print("\nThe shape of Y: ", train_y.shape)
 
-    training_epochs = 50
+    training_epochs = 5000
     n_dim = train_x.shape[1]
     n_classes = 3
     n_hidden_units_one = 280
@@ -92,13 +127,13 @@ def MNN(train_x, train_y, test_x, test_y):
     W = tf.Variable(tf.random_normal([n_hidden_units_two, n_classes], mean=0,
                                      stddev=sd))
     b = tf.Variable(tf.random_normal([n_classes], mean=0, stddev=sd))
-    y_ = tf.nn.softmax(tf.matmul(h_2, W) + b)
+    hypothesis = tf.nn.softmax(tf.matmul(h_2, W) + b)
 
-    cost_function = -tf.reduce_sum(Y * tf.log(y_))
+    cost_function = -tf.reduce_sum(Y * tf.log(hypothesis))
     optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(
         cost_function)
 
-    correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(Y, 1))
+    correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     y_true, y_pred = None, None
@@ -108,29 +143,35 @@ def MNN(train_x, train_y, test_x, test_y):
         for epoch in range(training_epochs):
             _, cost = sess.run([optimizer, cost_function],
                                feed_dict={X: train_x, Y: train_y})
-            print("Current Cost: ", cost)
+            if epoch % 1000 == 0:
+                print("Current Cost: ", cost)
 
-        y_pred = sess.run(tf.argmax(y_, 1), feed_dict={X: test_x})
+        y_pred = sess.run(tf.argmax(hypothesis, 1), feed_dict={X: test_x})
         y_true = sess.run(tf.argmax(test_y, 1))
 
         print("Test accuracy: ", round(session.run(accuracy,
-                                                   feed_dict={X: ts_features,
-                                                              Y: ts_labels}),
+                                                   feed_dict={X: test_x,
+                                                              Y: test_y}),
                                        3))
 
 
 def main():
     print("Reading Files...")
-    X, y_temp = parse_train_files_to_np()
-    y = one_hot_encode(y_temp)
-    print("Y is now: ", y)
-    print("X is now: ", X)
+
+    trainX, train_y_temp = parse_train_files_to_np()
+    train_y = one_hot_encode(train_y_temp)
+
+    testX, test_y_temp = parse_test_files_to_np()
+    test_y = one_hot_encode(test_y_temp)
+
+    print("Y is now: ", test_y)
+    print("X is now: ", testX)
     print("Y shape is: ", y.shape)
     print("X shape is: ", X.shape)
 
     print("Done Reading!!!")
     print("Training MNN...")
-    MNN(X, y, None, None)
+    MNN(trainX, train_y, testX, test_y)
 
 
 main()
