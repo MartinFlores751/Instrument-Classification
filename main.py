c41c0685deb8f556a3fcbf8c58068625524e54cf
Martin Jaime Flores Jr <mf34314@gmail.com>
2018-11-29 01:26:56 -0600

    Refactor, update Pipfile

--- main.py
+++ main.py
@@ -1,180 +1,36 @@
-import os
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
-from skmultilearn.adapt import MLkNN
-from skmultilearn.dataset import load_dataset
-# from skmultilearn.dataset import save_to_arff
+from tools.dataset import FeatureExtractor
 
-import librosa
-import essentia
-from essentia.streaming import *
 import numpy as np
+import pandas as pd
 import tensorflow as tf
-import re
 
 np.set_printoptions(threshold=np.inf)
 
-trainPath = os.environ['IRMAS_TRAIN']
-testPath = os.environ['IRMAS_TEST']
 # Possible sets are:
 # ('cel/', 'cla/', 'flu/', 'gac/', 'gel/', 'org/', 'pia/', 'sax/', 'tru/', 'vio/', 'voi/')
 
-# Pia, Gel, and Voi have the most training samples, so let's use those
-# Replace Voi with Sax if you want pure instruments
+# Current train size: 2107
+# Current test size: 1357
 
-trainFolders = ('gel/', 'pia/', 'voi/')
-testFolders = ('Part1/', 'Part2/')  #('Part3/')
-num_classes = len(trainFolders)
-
-
-def list_files(path):
-    return tuple(os.listdir(path))
-
-
-def extract_features(file, folder):
-    full_file_path = folder + file
-
-    file_loader = MonoLoader(filename=full_file_path)
-    frameCutter = FrameCutter(frameSize=1024, hopSize=512)
-    w = Windowing(type='hann')
-
-    spec = Spectrum()
-    specCont = SpectralContrast()
-    mfcc = MFCC()
-
-    pool = essentia.Pool()
-
-    file_loader.audio >> frameCutter.signal
-    frameCutter.frame >> w.frame >> spec.frame
-
-    spec.spectrum >> mfcc.spectrum
-    mfcc.bands >> (pool, 'lowlevel.mel_bands')
-    mfcc.mfcc >> (pool, 'lowlevel.mfcc')
-
-    essentia.run(file_loader)
-
-    return pool['lowlevel.mfcc'], pool['lowlevel.mel_bands']
-
-
-def get_labels_from_name(file):
-    return re.findall(r"\[([A-Za-z0-9_]+)\]", file)
-
-
-def get_label_from_txt(file_path):
-    """
-    Reads text from file at file_path
-    Uses first line as label
-    """
-    labels = []
-
-    with open(file_path, "r") as file:
-        for line in file:
-            labels.append(line.strip('\t\n'))
-
-    return labels
-
-
-def parse_train_files_to_np():
-    """
-    Reads trainPath and tainFolders to parse traning files
-    """
-    data = np.empty((0, 53))
-    labels = np.empty((0, num_classes))
-    for folder in trainFolders:
-        files_in_folder = list_files(trainPath + folder)
-        print("Extracting data for the " + folder[:-1] + " instrument.")
-        for file in files_in_folder:
-            file_label = get_labels_from_name(file)
-            file_label = one_hot_encode(file_label)
-
-            mfccs, mel_bands = extract_features(file, trainPath + folder)
-            mfccs = np.mean(mfccs, axis=0)
-            mel_bands = np.mean(mel_bands, axis=0)
-
-            features = np.hstack([mfccs, mel_bands])
-            data = np.vstack([data, features])
-            labels = np.vstack((labels, file_label))
-    return data, labels
-
-
-def parse_test_files_to_np():
-    """
-    Reads testPath and testFolder to parse test folders
-    """
-    data = np.empty((0, 53))
-    labels = np.empty((0, num_classes))
-
-    for folder in testFolders:
-        files_in_folder = list_files(testPath + folder)
-        print("Extracting data from the " + folder + " folder.")
-
-        proper_files = []
-
-        for file in files_in_folder:
-            if file.endswith(".txt"):
-                proper_files.append(file[:-4])
-
-        for file in proper_files:
-            file_label = get_label_from_txt(testPath + folder + file + ".txt")
-            isValid = False
-
-            for train in trainFolders:
-                for label in file_label:
-                    if train[:-1] == label:
-                        isValid = True
-                        break
-
-            if not isValid:
-                continue
-
-            mfccs, bands = extract_features(file + ".wav", testPath + folder)
-            mfccs = np.mean(mfccs, axis=0)
-            bands = np.mean(bands, axis=0)
-
-            features = np.hstack([mfccs, bands])
-            features = np.hstack([mfccs, bands])
-            data = np.vstack([data, features])
-            labels = np.vstack([labels, one_hot_encode(file_label)])
-
-    return data, labels
+train_folders = ('gel/', 'pia/', 'sax/')
+test_folders = ('Part1/', 'Part2/', 'Part3/')
+num_classes = len(train_folders)
 
 
 def one_hot_encode(labels):
     valid_labels = []
-    for folder in trainFolders:
+    for folder in train_folders:
         valid_labels.append(folder[:-1])
 
     enc = LabelBinarizer()
     enc.fit(valid_labels)
-    pre_labels = enc.transform(labels)
+    labels = enc.transform(labels)
 
-    final_label = np.zeros(num_classes)
-
-    for label in pre_labels:
-        for valid_label in trainFolders:
-            if np.array_equal(label, enc.transform([valid_label[:-1]]).flatten()):
-                final_label = np.add(label, final_label)
-
-    return final_label
-
-
-def isGood(result):
-    num_rows = 0
-    num_good = 0.0
-    for row in result:
-        num_rows += 1
-        good = False
-        for col in result:
-            for elem in col:
-                if elem is True:
-                    good = True
-                    break
-
-        if good is True:
-            num_good += 1
-
-    return num_good / num_rows
+    return label
 
 
 def MNN(train_x, train_y, test_x, test_y):
@@ -226,50 +82,63 @@ def MNN(train_x, train_y, test_x, test_y):
                 print("Raw:\n", raw)
                 print("Compare is:\n", inter)
                 print("Accuracy is: ", acc, "%")
-                print("True Acc is: ", isGood(inter))
                 print("Current Cost: ", cost)
                 print("Current Iter: ", epoch)
 
 
-def ML_KNN(train_x, train_y, test_x, test_y):
-    knn = MLkNN(k=2)
-    knn.fit(train_x, train_y)
-    prediction = knn.predict(test_x)
-    metrics.hamming_loss(test_y, prediction)
+def make_csv():
+    # Set features
+    
+    features = ["mfcc_1"]
+    for i in range(2, 14):
+        features.append("mfcc_" + str(i))
 
+    for i in range(1, 41):
+        features.append("mel_" + str(i))
 
-def RNN(train_x, train_y, test_x, test_y):
-    num_epochs = 100
-    total_series_length = 50000
-    truncated_backprop_length = 15
-    state_size = 4
-    num_classes = 2
-    echo_step = 3
-    batch_size = 5
-    num_batches = total_series_length//batch_size//truncated_backprop_length
+    for i in range(1, 4):
+        features.append("class_" + str(i))
 
-    return None
+    # Generate Features
+    fe = FeatureExtractor(train_folders=train_folders, test_folders=test_folders)
+    train_X, test_X, train_y, test_y = fe.load_test_train_data()
 
+    train = np.hstack([train_X, train_y])
+    test = np.hstack([test_X, test_y])
 
-def save_data_arff(X, y, f_name):
-    save_to_arff(X, y, label_location='start', save_sparse=True, filename=f_fname)
+    irmas_all = np.vstack([train, test])
+    
+    data = pd.DataFrame(data=irmas_all, columns=features)
+    data.to_csv("data/gel-pia-sax[MFCC][MEL][53].csv", index=False)
 
-
-def load_data_arff(file_path):
-    return None
 
 
 def main():
     print("Reading Files...")
 
-    # Generate Data
-    trainX, train_y = parse_train_files_to_np()
-    testX, test_y = parse_test_files_to_np()
+    # Load Features
+    data = pd.read_csv("test.csv")
 
+    print("Done!\nProcessing files...")
+    
+    # Split data
+    train = data[:2107]
+    test = data[2107:]
+
+    train_X = train.drop(["class_1", "class_2", "class_3"], axis=1)
+    train_y = train[["class_1", "class_2", "class_3"]]
+
+    test_X = test.drop(["class_1", "class_2", "class_3"], axis=1)
+    test_y = test[["class_1", "class_2", "class_3"]]
+    
+    # Binary Encoding!!!
+    train_y = one_hot_encode(train_y)
+    test_y = one_hot_encode(test_y)
+    
     # Scale Data
-    mms = MinMaxScaler()
-    trainX = mms.fit_transform(trainX)
-    testX = mms.transform(testX)
+    ss = StandardScaler()
+    trainX = ss.fit_transform(trainX)
+    testX = ss.transform(testX)
 
     # Shuffle the training data just in case...
     train = np.append(trainX, train_y, axis=1)
@@ -280,21 +149,10 @@ def main():
     train_y = train[:, -3:]
     print(train_y.shape)
 
-    # Save Data
-    # save_data_arff(trainX, train_y, "train_data.arff")
-    # save_data_arff(testX, test_y, "test_data.arff")
-
-    # Load Data
-    # load_data_arff()
-
-    print("Train X:\n", trainX.shape)
-    print("Train Y:\n", train_y.shape)
-
-    print("Done Reading!!!")
+    print("Done Processing!!!")
     print("Training MNN...")
 
-    MNN(trainX, train_y, testX, test_y)
-    # ML_KNN(trainX, train_y, testX, test_y)
+    MNN(train_X, train_y, test_X, test_y)
 
 
 main()
---
+++ tools/dataset.py
@@ -0,0 +1,158 @@
+import essentia
+import numpy as np
+from essentia.streaming import *
+import os
+import re
+
+
+class FeatureExtractor():
+    
+    def __init__(self, train_folders=None, test_folders=None):
+        self.train_path = os.environ['IRMAS_TRAIN']
+        self.test_path = os.environ['IRMAS_TEST']
+        self.train_folders = train_folders
+        self.test_folders = test_folders
+        self.num_classes = len(train_folders)
+
+        self.train_X = None
+        self.test_X = None
+        self.train_y = None
+        self.test_y = None
+
+
+    def __get_label_from_txt(self, file_path):
+        """
+        Reads text from file at file_path
+        Uses first line as label
+        """
+        labels = []
+
+        with open(file_path, "r") as file:
+            for line in file:
+                labels.append(line.strip('\t\n'))
+
+        return labels
+
+
+        
+    def __get_labels_from_name(self, file):
+        return re.findall(r"\[([A-Za-z0-9_]+)\]", file)
+
+
+    def __list_files(self, path):
+        return tuple(os.listdir(path))
+
+    def __extract_features(self, file, folder):
+        full_file_path = folder + file
+        
+        file_loader = MonoLoader(filename=full_file_path)
+        frameCutter = FrameCutter(frameSize=1024, hopSize=512)
+        w = Windowing(type='hann')
+
+        spec = Spectrum()
+        specCont = SpectralContrast()
+        mfcc = MFCC()
+
+        pool = essentia.Pool()
+
+        file_loader.audio >> frameCutter.signal
+        frameCutter.frame >> w.frame >> spec.frame
+
+        spec.spectrum >> mfcc.spectrum
+        mfcc.bands >> (pool, 'lowlevel.mel_bands')
+        mfcc.mfcc >> (pool, 'lowlevel.mfcc')
+
+        essentia.run(file_loader)
+
+        return pool['lowlevel.mfcc'], pool['lowlevel.mel_bands']
+
+
+    def load_training_data(self):
+        """
+        Reads trainPath and tainFolders to parse traning files
+        """
+        data = np.empty((0, 53))
+        labels = np.empty((0, self.num_classes))
+        for folder in self.train_folders:
+            files_in_folder = self.__list_files(self.train_path + folder)
+            for file in files_in_folder:
+                file_label = self.__get_labels_from_name(file)
+
+                for label in list(file_label):
+                    if label + "/" in self.train_folders:
+                        continue
+                    else:
+                        file_label.remove(label)
+                
+                while len(file_label) < self.num_classes:
+                    file_label.append('')
+
+                mfccs, mel_bands = self.__extract_features(file, self.train_path + folder)
+                mfccs = np.mean(mfccs, axis=0)
+                mel_bands = np.mean(mel_bands, axis=0)
+
+                features = np.hstack([mfccs, mel_bands])
+                data = np.vstack([data, features])
+                labels = np.vstack((labels, file_label))
+
+        self.train_X = data
+        self.train_y = labels
+        return data, labels
+
+
+    def load_testing_data(self):
+        """
+        Reads testPath and testFolder to parse test folders
+        """
+        data = np.empty((0, 53))
+        labels = np.empty((0, self.num_classes))
+
+        for folder in self.test_folders:
+            files_in_folder = self.__list_files(self.test_path + folder)
+           
+            proper_files = []
+
+            for file in files_in_folder:
+                if file.endswith(".txt"):
+                    proper_files.append(file[:-4])
+
+            for file in proper_files:
+                file_label = self.__get_label_from_txt(self.test_path + folder + file + ".txt")
+                isValid = False
+
+                for train in self.train_folders:
+                    for label in file_label:
+                        if train[:-1] == label:
+                            isValid = True
+                            break
+
+                if not isValid:
+                    continue
+
+                mfccs, bands = self.__extract_features(file + ".wav", self.test_path + folder)
+                mfccs = np.mean(mfccs, axis=0)
+                bands = np.mean(bands, axis=0)
+
+                for label in list(file_label):
+                    if label + "/" in self.train_folders:
+                        continue
+                    else:
+                        file_label.remove(label)
+                
+                while len(file_label) < 3:
+                    file_label.append('')
+
+                features = np.hstack([mfccs, bands])
+                data = np.vstack([data, features])
+                labels = np.vstack([labels, file_label])
+
+        self.test_X = data
+        self.test_y = labels
+        return data, labels
+
+
+    def load_test_train_data(self):
+        self.load_training_data()
+        self.load_testing_data()
+        return self.train_X, self.test_X, self.train_y, self.test_y
+
